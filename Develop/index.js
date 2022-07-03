974ef11268d059e6ee0113cc84ecf7a4631124ea
Katie <katiebutler37@gmail.com>
2022-07-03 15:10:07 -0400

    inquirer bug fixed

--- Develop/index.js
+++ Develop/index.js
@@ -3,6 +3,8 @@ const fs = require('fs');
 const inquirer = require('inquirer');
 const generateMarkdown = require('./utils/generateMarkdown.js');
 
+let data = [];
+
 // TODO: Create an array of questions for user input
 const promptDescription = () => {
     return inquirer
@@ -26,10 +28,10 @@ const promptDescription = () => {
                 message: "What was the motivation for creating this repository? (Required)",
                 validate: motivationInput => {
                     if (motivationInput) {
-                    return true;
+                        return true;
                     } else {
-                    console.log('You need to enter an explanation for your repository!');
-                    return false;
+                        console.log('You need to enter an explanation for your repository!');
+                        return false;
                     }
                 }
             },
@@ -46,10 +48,10 @@ const promptDescription = () => {
                 message: "What did you learn from creating this project? (Required)",
                 validate: learnInput => {
                     if (learnInput) {
-                    return true;
+                        return true;
                     } else {
-                    console.log('You need to write about what you learned from creating your repository!');
-                    return false;
+                        console.log('You need to write about what you learned from creating your repository!');
+                        return false;
                     }
                 }
             },
@@ -58,13 +60,13 @@ const promptDescription = () => {
                 name: "installationFirstStep",
                 message: "What is the first step required to install your project? (Required)",
                 validate: firstStepInput => {
-                if (firstStepInput) {
-                    return true;
-                  } else {
-                    console.log('You need to enter the first step to install your project!');
-                    return false;
-                  }
-                }  
+                    if (firstStepInput) {
+                        return true;
+                    } else {
+                        console.log('You need to enter the first step to install your project!');
+                        return false;
+                    }
+                }
             },
             {
                 type: "confirm",
@@ -73,106 +75,110 @@ const promptDescription = () => {
                 default: false,
             }
         ])
-        .then(descriptionData => {
-            data.push(descriptionData);
-            if (descriptionData.confirmAddSteps) {
-                promptInstallationNextSteps(data);
-              } else {
-                return data;
-              }
-        })
-    };
+};
 
 const promptInstallationNextSteps = data => {
+    console.log ("next steps");
     // If there's no 'next steps' array property, create one
     if (!data.nextSteps) {
         data.nextSteps = [];
-      }
+    }
     return inquirer
-    .prompt([
-        {
-            type: "input",
-            name: "installationNextStep",
-            message: "What is the next step required to install your project? (Required)",
-            validate: nextStepInput => {
-            if (nextStepInput) {
-                return true;
-              } else {
-                console.log('You need to enter the next step to install your project!');
-                return false;
-              }
-            }  
-        },
-        {
-            type: "confirm",
-            name: "confirmAddStep",
-            message: "Are there more steps to install your project?",
-            default: false
-        },
-    ])
-    .then(installationNextStepsData => {
-        data.nextSteps.push(installationNextStepsData);
-        if (installationNextStepsData.confirmAddStep) {
-            return promptInstallationNextSteps(data);
-          } else {
-            return data;
-          }
-    })
+        .prompt([
+            {
+                type: "input",
+                name: "installationNextStep",
+                message: "What is the next step required to install your project? (Required)",
+                validate: nextStepInput => {
+                    if (nextStepInput) {
+                        return true;
+                    } else {
+                        console.log('You need to enter the next step to install your project!');
+                        return false;
+                    }
+                }
+            },
+            {
+                type: "confirm",
+                name: "confirmAddStep",
+                message: "Are there more steps to install your project?",
+                default: false
+            },
+        ])
+        .then(installationNextStepsData => {
+            data.nextSteps.push(installationNextStepsData);
+            if (installationNextStepsData.confirmAddStep) {
+                return promptInstallationNextSteps(data);
+            } else {
+                promptUsage(data);
+            }
+        })
 };
 
 const promptUsage = data => {
     return inquirer
-    .prompt([
-        {
-            type: "input",
-            name: "instructions",
-            message: "Provide brief instructions for use. (Required)",
-            validate: instructionsInput => {
-                if (instructionsInput) {
-                    return true;
-                  } else {
-                    console.log('You need to enter the instructions for the use of your repository!');
-                    return false;
-                  }
-                }  
-        },
-        {
-            type: "confirm",
-            name: "confirmScreenshot",
-            message: "Would you like to include a screenshot to demonstrate?",
-            default: true
-        },
-        {
-          type: 'input',
-          name: 'screenshot',
-          message: 'Please enter the file name of the screenshot.',
-          when: ({ confirmScreenshot }) => confirmScreenshot
-        }
-    ]);
+        .prompt([
+            {
+                type: "input",
+                name: "instructions",
+                message: "Provide brief instructions for use. (Required)",
+                validate: instructionsInput => {
+                    if (instructionsInput) {
+                        return true;
+                    } else {
+                        console.log('You need to enter the instructions for the use of your repository!');
+                        return false;
+                    }
+                }
+            },
+            {
+                type: "confirm",
+                name: "confirmScreenshot",
+                message: "Would you like to include a screenshot to demonstrate?",
+                default: true
+            },
+            {
+                type: 'input',
+                name: 'screenshot',
+                message: 'Please enter the file name of the screenshot.',
+                when: ({ confirmScreenshot }) => confirmScreenshot
+            }
+        ]);
 };
 
 promptDescription()
-    .then(promptUsage(data))
-    .then((data) => {
-        // Use user feedback for... whatever!!
-      })
-      .catch((error) => {
-        if (error.isTtyError) {
-          // Prompt couldn't be rendered in the current environment
+    .then(descriptionData => {
+        console.log(descriptionData);
+        data.push(descriptionData);
+        console.log("hello");
+        if (descriptionData.confirmAddSteps) {
+            console.log("add steps");
+            promptInstallationNextSteps(data);
         } else {
-          // Something else went wrong
+            promptUsage(data);
         }
-      });
+    })
+    // promptUsage(data)
+    // .then((data) => {
+    //     // Use user feedback for... whatever!!
+    // })
+    .catch((error) => {
+        if (error.isTtyError) {
+            // Prompt couldn't be rendered in the current environment
+        } else {
+            // Something else went wrong
+        }
+    });
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
     fs.writeFile('README.md', generateMarkdown(), err => {
         if (err) throw err;
-      });
+    });
 }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() { }
 
 // Function call to initialize app
 init();
